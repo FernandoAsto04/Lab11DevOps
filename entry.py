7844e42c908ce3b050b2f4af70b939d95432f67b
FernandoAsto04 <20214543@aloe.ulima.edu.pe>
2025-07-02 21:02:21 -0500

    commit 1
    Comentario para hacer que se lance

--- entry.py
+++ entry.py
@@ -2,6 +2,7 @@ import json
 from core import payload
 from workers import Response
 
+#Comentario para el commit
 def on_fetch(request):
     return Response(json.dumps(payload()),
                     headers={"content-type": "application/json"})
